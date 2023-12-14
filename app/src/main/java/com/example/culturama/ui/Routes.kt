748f4b0d1abffd0695e4bb016498131b5d8963e8
septiabela <septia.bela@students.utdi.ac.id>
2023-12-14 17:15:47 +0700

    Menambah Routes.kt

--- app/src/main/java/com/example/culturama/ui/Routes.kt
+++ app/src/main/java/com/example/culturama/ui/Routes.kt
@@ -4,4 +4,6 @@ sealed class Routes(val route: String) {
     object SignUp : Routes("SignUp")
     object ForgotPassword : Routes("ForgotPassword")
     object Login : Routes("Login")
+    object Dashboard : Routes("Dashboard")
+    object Home : Routes("Home")
 }
