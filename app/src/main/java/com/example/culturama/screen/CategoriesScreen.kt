283a0f4a86463a4dfaf6b5996180f2ac515128a5
septiabela <septia.bela@students.utdi.ac.id>
2023-12-16 16:16:41 +0700

    menambahkan CategoriesScreen.kt

--- app/src/main/java/com/example/culturama/screen/CategoriesScreen.kt
+++ app/src/main/java/com/example/culturama/screen/CategoriesScreen.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
