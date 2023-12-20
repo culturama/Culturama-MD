16988752324ee1506d6b031bfb580c8e677a099b
septiabela <septia.bela@students.utdi.ac.id>
2023-12-20 19:26:33 +0700

    favoritescreen

--- app/src/main/java/com/example/culturama/ui/screen/fav/komponen/Search.kt
+++ app/src/main/java/com/example/culturama/ui/screen/fav/komponen/Search.kt
@@ -1,4 +1,4 @@
-package com.example.culturama.ui.fav.komponen
+package com.example.culturama.ui.screen.fav.komponen
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
