e321f83f71ea0a903ef5c33ec857bbd7a1afe0db
Ariq <ariqfadhil2015@gmail.com>
2023-12-22 04:08:02 +0700

    change color

--- app/src/main/java/com/example/culturama/ui/theme/Color.kt
+++ app/src/main/java/com/example/culturama/ui/theme/Color.kt
@@ -10,7 +10,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Teal200 = Color(0xFF7BD4CE)
+val Teal300 = Color(0xFF7BD4CE)
+val Teal100 = Color(0xFFDDFFFD)
 val TealGrey = Color(0xFF5D7271)
 
 val NavyTeal = Color(0xFF04364A)
