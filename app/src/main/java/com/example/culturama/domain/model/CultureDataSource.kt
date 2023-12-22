1e6214d8b4aa0edfbde003cc92ba2797ce278303
septiabela <septia.bela@students.utdi.ac.id>
2023-12-22 09:56:49 +0700

    change

--- app/src/main/java/com/example/culturama/domain/model/CultureDataSource.kt
+++ app/src/main/java/com/example/culturama/domain/model/CultureDataSource.kt
@@ -6,6 +6,6 @@ import com.example.culturama.R
 object CultureDataSource {
     val listCulture = listOf(
         Culture(1, "Candi Borobudur", "Destinasi Turis", 4, R.drawable.onboarding_1, 50000, "Ini Candi Corobudur ya ges ya"),
-        Culture(2, "Adat Bali", "Tradisi Lokal", 4, R.drawable.onboarding_2, 0, "Ini tradisi lokal ya ges ya")
+        Culture(2, "Adat Ponorogo", "Tradisi Lokal", 4, R.drawable.onboarding_2, 0, "Ini tradisi lokal ya ges ya")
     )
 }
